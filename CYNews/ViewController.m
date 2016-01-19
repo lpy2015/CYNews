6aa99d04f7ceb1c24e72afd5a25120f53c5f1aab
qianfeng001 <qianfeng001@14k.local>
2016-01-19 16:01:32 +0800

    Initial Commit

---
+++ CYNews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CYNews
+//
+//  Created by qianfeng001 on 16/1/19.
+//  Copyright © 2016年 陈宇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
